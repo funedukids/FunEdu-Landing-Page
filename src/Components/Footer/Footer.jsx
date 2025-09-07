5eb480dfa3b3e05fa67fe04d1a0916c0d21ad2f3
Adejayan Oluwatosin <holuwarthosyn21@gmail.com>
2025-09-08 00:19:42 +0100

    footer icon

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -21,26 +21,26 @@ function Footer() {
               {/* <li className="mb-2"><a href="#" className="text-white link-decoration-custom"><img src={img1} width={35} alt="facebook" /> Facebook</a></li> */}
               {/* <li className="mb-2"><a href="#" className="text-white link-decoration-custom"><img src={img2} width={35} alt="linkedin" /> LinkedIn</a></li> */}
               <li className="icon mb-2"><a href="https://www.instagram.com/funedumobilekidapp?igsh=bXY0Z3drZGhrMmNw" className="text-black link-decoration-custom"><img src={img3} width={35} alt="instagram" /> Instagram</a></li>
-              <li className="mb-2"><a href="tech@funedukids.com" className="text-black link-decoration-custom"><img src={img4} width={35} alt="email" style={{ filter: 'invert(0%) sepia(1000%) saturate(10000%) hue-rotate(500deg)' }} />Email</a></li>
-              <li className="mb-2"><a href="https://x.com/FuneduKidApp?t=JIWXdfiR7d8RU3RvG1PD8g&s=09" className="text-black link-decoration-custom"><img src={img5} width={35} alt="x" /> X</a></li>
+              <li className="icon mb-2"><a href="tech@funedukids.com" className="text-black link-decoration-custom"><img src={img4} width={35} alt="email" style={{ filter: 'invert(0%) sepia(1000%) saturate(10000%) hue-rotate(500deg)' }} />Email</a></li>
+              <li className="icon mb-2"><a href="https://x.com/FuneduKidApp?t=JIWXdfiR7d8RU3RvG1PD8g&s=09" className="text-black link-decoration-custom"><img src={img5} width={35} alt="x" /> X</a></li>
             </ul>
           </div>
           <div className="col-md-3">
             <h4>Products</h4>
             <ul className="list-unstyled">
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Overview</a></li>
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Learn Anywhere</a></li>
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Creative Play</a></li>
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Parental Tools</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Overview</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Learn Anywhere</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Creative Play</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Parental Tools</a></li>
             </ul>
           </div>
           <div className="col-md-3">
             <h4>Policies</h4>
             <ul className="list-unstyled">
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Terms of Service</a></li>
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Privacy Policy</a></li>
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Data Security & Compliance</a></li>
-              <li className="mb-2"><a href="#" className="text-white link-decoration-custom">Information Sales</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Terms of Service</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Privacy Policy</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Data Security & Compliance</a></li>
+              <li className="mb-2"><a href="#" className="text-black link-decoration-custom">Information Sales</a></li>
             </ul>
           </div>
         </div>
